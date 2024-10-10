2c53f8e65a54fc566bc72b777d88cc232327f62c
aiinty <thisissparta2016@mail.ru>
2024-10-10 18:22:38 +0600

    Initial commit

---
+++ app/src/main/java/com/aiinty/morsekey/IMEService.kt
@@ -0,0 +1,40 @@
+package com.aiinty.morsekey
+
+import android.view.View
+import androidx.lifecycle.*
+import androidx.savedstate.SavedStateRegistry
+import androidx.savedstate.SavedStateRegistryController
+import androidx.savedstate.SavedStateRegistryOwner
+import androidx.savedstate.setViewTreeSavedStateRegistryOwner
+
+class IMEService : LifecycleInputMethodService(),
+    ViewModelStoreOwner,
+    SavedStateRegistryOwner {
+
+    override fun onCreateInputView(): View {
+        val view = ComposeKeyboardView(this)
+
+        window?.window?.decorView?.let { decorView ->
+            decorView.setViewTreeLifecycleOwner(this)
+            decorView.setViewTreeViewModelStoreOwner(this)
+            decorView.setViewTreeSavedStateRegistryOwner(this)
+        }
+        return view
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        savedStateRegistryController.performRestore(null)
+    }
+
+    override val viewModelStore: ViewModelStore
+        get() = store
+    override val lifecycle: Lifecycle
+        get() = dispatcher.lifecycle
+
+    private val store = ViewModelStore()
+
+    private val savedStateRegistryController = SavedStateRegistryController.create(this)
+
+    override val savedStateRegistry: SavedStateRegistry get() = savedStateRegistryController.savedStateRegistry
+}
---
+++ app/src/main/java/com/aiinty/morsekey/KeyboardScreen.kt
@@ -0,0 +1,44 @@
+package com.aiinty.morsekey
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.unit.Dp
+
+@Composable
+fun KeyboardScreen() {
+    Column(
+        modifier = Modifier
+            .background(Color(0xFF9575CD))
+            .fillMaxWidth()
+    ) {
+        // TODO
+    }
+}
+
+@Composable
+fun FixedHeightBox(modifier: Modifier, height: Dp, content: @Composable () -> Unit) {
+    Layout(modifier = modifier, content = content) { measurables, constraints ->
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints)
+        }
+        val h = height.roundToPx()
+        layout(constraints.maxWidth, h) {
+            placeables.forEach { placeable ->
+                placeable.place(x = 0, y = kotlin.math.min(0, h - placeable.height))
+            }
+        }
+    }
+}
+
+@Composable
+fun KeyboardKey(
+    keyboardKey: String,
+    modifier: Modifier
+) {
+    // TODO
+}
